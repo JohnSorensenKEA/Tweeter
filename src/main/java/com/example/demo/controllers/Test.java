469ec048b70928d34a3a867fd782779489cc9198
John <john383p@stud.kea.dk>
2021-02-15 19:41:16 +0100

    Test things for EC2 test

--- src/main/java/com/example/demo/controllers/Test.java
+++ src/main/java/com/example/demo/controllers/Test.java
@@ -1,4 +1,18 @@
 package com.example.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Test {
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/test";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
 }
